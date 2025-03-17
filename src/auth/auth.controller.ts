d5d28fea9b1575afa7d0b0901832521067ed81ae
solone2003 <nhanv841@gmail.com>
2025-03-17 18:43:13 +0700

    commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service'; 
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  @Post('google-login')
+  async googleLogin(@Body('token') token: string) {
+    const decodedToken = await this.authService.verifyToken(token);
+
+    const payload = {
+        uid: decodedToken.uid,
+        email: decodedToken.email || 'no-email@example.com',
+        name: decodedToken.displayName || decodedToken.email || 'Unknown User', // Sửa `name` -> `displayName`
+        picture: decodedToken.picture || null, 
+    };
+
+    const jwt = this.authService.generateJwt(payload);
+    return { jwt, user: payload };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+import { Injectable} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private jwtService: JwtService,
+    private configService: ConfigService) {
+    const firebaseCredentialsPath = this.configService.get<string>('FIREBASE_CREDENTIALS');
+
+    try {
+      if (!firebaseCredentialsPath) {
+        throw new Error('FIREBASE_CREDENTIALS is not defined in environment variables');
+      }
+
+      const fileContent = fs.readFileSync(firebaseCredentialsPath, 'utf8');
+      const serviceAccount = JSON.parse(fileContent);
+
+      if (!admin.apps.length) {
+        admin.initializeApp({
+          credential: admin.credential.cert(serviceAccount),
+        });
+        console.log('Firebase Admin Initialized Successfully');
+      }
+    } catch (error) {
+      console.error('Error initializing Firebase Admin:', error.message);
+    }
+  }
+
+  async verifyToken(idToken: string) {
+    console.log('🔍 Token nhận được:', idToken);
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    return decodedToken;
+  }
+  
+  
+
+  generateJwt(payload: { uid: string; email: string; name: string }) {
+    return this.jwtService.sign(payload);
+  }
+}
